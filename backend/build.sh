00680c08a503b269fd1035f40e53795cdf463fd2
Janish10 <janishandrin75@gmail.com>
2026-08-19 11:50:30 +0530

    Run db migrations and weight download in build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,3 +4,5 @@ set -e
 pip install --upgrade pip
 pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
+python download_weights.py
+flask db upgrade
