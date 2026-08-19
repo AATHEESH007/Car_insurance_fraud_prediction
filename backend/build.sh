9d7fabbe48f106bccfa7ffdadfe8bcbf40b50d15
Janish10 <janishandrin75@gmail.com>
2026-08-19 11:05:40 +0530

    Add build script with pip upgrade for Render deployment

---
+++ backend/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+pip install --upgrade pip
+pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cpu
+pip install -r requirements.txt
